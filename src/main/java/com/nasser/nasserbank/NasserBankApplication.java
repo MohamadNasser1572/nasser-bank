9b9f7b378e7dc666b8382d381e8733e6e26c786d
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-02 15:52:00 +0300

    add swagger UI dependency + change the UI design for the info of the project in swagger

--- src/main/java/com/nasser/nasserbank/NasserBankApplication.java
+++ src/main/java/com/nasser/nasserbank/NasserBankApplication.java
@@ -1,9 +1,34 @@
 package com.nasser.nasserbank;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Nasser Bank App",
+                description = "Backend Rest APIs for Nasser Bank",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Mohammad Nasser",
+                        email = "mhmdnaser1572@gmail.com",
+                        url = "https://github.com/MohamadNasser1572/nasser-bank"
+                ),
+                license = @License(
+                        name = "Mohammad Nasser",
+                        url = "https://github.com/MohamadNasser1572/nasser-bank"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Nasser Bank App Documentation",
+                url = "https://github.com/MohamadNasser1572/nasser-bank"
+        )
+)
 public class NasserBankApplication {
 
     public static void main(String[] args) {
