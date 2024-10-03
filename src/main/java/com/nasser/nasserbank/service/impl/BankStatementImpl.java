f3cbb2a51c18fa42d1986a37151f27a668623b6c
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-10-03 20:22:17 +0300

    add creation date and modification date

---
+++ src/main/java/com/nasser/nasserbank/service/impl/BankStatementImpl.java
@@ -0,0 +1,10 @@
+package com.nasser.nasserbank.service;
+
+
+public class BankStatement {
+    /*
+    * retrieve list of transactions within a date range given an account number
+    * generate a pdf file of transactions
+    * send the file via email
+    * */
+}
