e73e0aa1cf87c244a09032a2ebc6282de687232c
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-12-22 11:15:11 +0200

    reformat main code

--- src/main/java/com/nasser/nasserbank/NasserBankApplication.java
+++ src/main/java/com/nasser/nasserbank/NasserBankApplication.java
@@ -9,30 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Nasser Bank App",
-                description = "Backend Rest APIs for Nasser Bank",
-                version = "v1.0",
-                contact = @Contact(
-                        name = "Mohammad Nasser",
-                        email = "mhmdnaser1572@gmail.com",
-                        url = "https://github.com/MohamadNasser1572/nasser-bank"
-                ),
-                license = @License(
-                        name = "Mohammad Nasser",
-                        url = "https://github.com/MohamadNasser1572/nasser-bank"
-                )
-        ),
-        externalDocs = @ExternalDocumentation(
-                description = "Nasser Bank App Documentation",
-                url = "https://github.com/MohamadNasser1572/nasser-bank"
-        )
-)
+@OpenAPIDefinition(info = @Info(title = "Nasser Bank App", description = "Backend Rest APIs for Nasser Bank", version = "v1.0", contact = @Contact(name = "Mohammad Nasser", email = "mhmdnaser1572@gmail.com", url = "https://github.com/MohamadNasser1572/nasser-bank"), license = @License(name = "Mohammad Nasser", url = "https://github.com/MohamadNasser1572/nasser-bank")), externalDocs = @ExternalDocumentation(description = "Nasser Bank App Documentation", url = "https://github.com/MohamadNasser1572/nasser-bank"))
 public class NasserBankApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(NasserBankApplication.class, args);
-    }
+        public static void main(String[] args) {
+                SpringApplication.run(NasserBankApplication.class, args);
+        }
 
 }
