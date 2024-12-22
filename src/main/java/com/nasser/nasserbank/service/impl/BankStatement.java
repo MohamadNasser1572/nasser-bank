57c14f8ababcb53c1ba901033479097ab104759b
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-12-22 11:17:24 +0200

    Add new BankStatement implementation for transaction management

--- src/main/java/com/nasser/nasserbank/service/impl/BankStatement.java
+++ src/main/java/com/nasser/nasserbank/service/impl/BankStatement.java
@@ -3,7 +3,6 @@ package com.nasser.nasserbank.service.impl;
 import com.nasser.nasserbank.entity.Transaction;
 import com.nasser.nasserbank.repository.TransactionRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -12,19 +11,21 @@ import java.util.List;
 
 @Component
 @AllArgsConstructor
-public class BankStatementImpl {
+public class BankStatement {
     private final TransactionRepository transactionRepository;
     /*
      * retrieve list of transactions within a date range given an account number
      * generate a pdf file of transactions
      * send the file via email
-     * */
+     */
 
     public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate) {
         LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
         LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
-        List<Transaction> transactionList = transactionRepository.findAll().stream().filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
-                .filter(transaction -> transaction.getCreatedAt().isEqual(start)).filter(transaction -> transaction.getCreatedAt().isEqual(end)).toList();
+        List<Transaction> transactionList = transactionRepository.findAll().stream()
+                .filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
+                .filter(transaction -> transaction.getCreatedAt().isEqual(start))
+                .filter(transaction -> transaction.getCreatedAt().isEqual(end)).toList();
 
         return transactionList;
     }
