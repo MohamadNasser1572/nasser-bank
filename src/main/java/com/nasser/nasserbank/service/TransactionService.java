941a382038b0db5888daf45d64eb1ae6c4bdb285
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-12-22 11:18:05 +0200

    remove unused import library

--- src/main/java/com/nasser/nasserbank/service/TransactionService.java
+++ src/main/java/com/nasser/nasserbank/service/TransactionService.java
@@ -1,7 +1,6 @@
 package com.nasser.nasserbank.service;
 
 import com.nasser.nasserbank.dto.TransactionDto;
-import com.nasser.nasserbank.entity.Transaction;
 
 public interface TransactionService {
     void saveTransaction(TransactionDto transactionDto);
